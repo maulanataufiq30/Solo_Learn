f551d41a41ccc7d827a9977986d2d16a93cb6014
maulanataufiq30 <taufiq.maulana0303@gmail.com>
2021-11-19 00:01:41 +0700

    part 1 done

---
+++ Leasson 6.1.py
@@ -0,0 +1,9 @@
+print(1 + 4*3)
+
+print ("I love Python")
+
+print((3**2)//2)
+
+print(100 // 42)
+
+print(10**5)
---
+++ Leasson 8.1.py
@@ -0,0 +1,7 @@
+print("Python is fun!")
+
+print('Always look on the bright side of life')
+
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+
+print('I\'m learning!')
