afd792ccb5547a87bd6013ecc2d4e387b4d876c1
maulanataufiq30 <taufiq.maulana0303@gmail.com>
2021-11-28 23:17:12 +0700

    part 2 solo learn

---
+++ Leasson 10.1.py
@@ -0,0 +1,11 @@
+print("spam" + 'eggs')
+
+print("2" + "2")
+
+print("Python" + "is" + "awesome")
+
+print("spam" *3)
+
+print(4 * '2')
+
+print(3*'7')
---
+++ Leasson 9.1.py
@@ -0,0 +1,14 @@
+print('One\nTwo\nThree')
+
+print("Hello \n World")
+
+print("""this
+is a
+mulltiline
+text""")
+
+
+print("Hi")
+print("""This
+is
+great""")
